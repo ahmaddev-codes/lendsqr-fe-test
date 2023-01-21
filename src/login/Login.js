82186d6f5f3318352f11124a56b392d08e5ccbff
Tijani Ahmad <omegabliss23@gmail.com>
2023-01-21 14:43:11 +0100

    Code: added login page component and style

---
+++ src/login/Login.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "./Login.scss";
+import Form from "./Form";
+import logo from "../../logo.svg";
+import vector from "../../assets/sign-in-vector.svg";
+
+const Login = () => {
+  return (
+    <main className="login">
+      <img className="logo" src={logo} alt="logo"></img>
+
+      <div className="login__content">
+        <img className="desktop" src={vector} alt="vector"></img>
+
+        <div className="details">
+          <div className="details__header">
+            <h1>Welcome!</h1>
+            <p>Enter details to login.</p>
+          </div>
+
+          <img className="mobile" src={vector} alt="vector"></img>
+
+          <Form />
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default Login;
